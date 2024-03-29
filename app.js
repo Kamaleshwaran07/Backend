652c9ab40a0de12ba3c6ae5aa681ab54c89b3497
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-03-29 17:38:33 +0530

    cleanup

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from 'cors';
 import userrouter from "./Routes/userrouter.min.js";
-import morgan from 'morgan'
-import cookieParser from "cookie-parser";
+// import morgan from 'morgan'
+// import cookieParser from "cookie-parser";
 const app = express()
 
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json())
 
-app.use(morgan("dev"))  
+// app.use(morgan("dev"))  
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
 app.use('/api/users', userrouter)
 
