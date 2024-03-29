5aa4b5a567ea69f075722b9cd391c310300220b1
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-03-29 17:47:12 +0530

    pol

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import userrouter from "./Routes/userrouter.min.js";
+import userrouter from "./Routes/userrouter.js";
 // import morgan from 'morgan'
 // import cookieParser from "cookie-parser";
 const app = express()
